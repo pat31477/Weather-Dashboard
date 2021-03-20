3dab9bbe8f7c5d7d73005413fe50b23acb0e317f
Pat31477 <pat31477@yahoo.com>
2021-03-20 09:05:50 -0400

    adding more js

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -163,7 +163,7 @@ $(document).ready(function(){
             method: 'GET',
           })
           .then(function(response) {
-            // Day 1
+            // 1st Day
             var iconCode = response.list[0].weather[0].icon;
             var iconURL = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
             $('#tempTwo').text(`Temp: ${parseInt(response.list[0].main.temp)}° F`);
@@ -172,18 +172,92 @@ $(document).ready(function(){
             $('#uvIndex').html(`UV Index: ${response.value}`);
             $('#weatherIconTwo').attr('src', iconUrl);
 
-            // Day 2
+            // 2nd Day
             var iconCode = response.list[8].weather[0].icon;
             var iconURL = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
-            $('#tempThree').text(`Temp: ${parseInt(response.list[0].main.temp)}° F`);
+            $('#tempThree').text(`Temp: ${parseInt(response.list[8].main.temp)}° F`);
             $('#iconThree').attr('src', iconURL);
             $('#humidThree').text(`Humidity: ${response.list[8].main.humidity}%`);
             $('#uvIndex').html(`UV Index: ${response.value}`);
             $('#weatherIconThree').attr('src' ,iconURL);
 
+            // 3rd Day
+            var iconCode = response.list[16].weather[0].icon;
+            var iconURL = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
+            $('#tempFour').text(`Temp: ${parseInt(response.list[16].main.temp)}° F`);
+            $('#iconFour').attr('src', iconURL);
+            $('#humidFour').text(`Humidity: ${response.list[16].main.humidity}%`);
+            $('#uvIndex').html(`UV Index: ${response.value}`);
+            $('#weatherIconFour').attr("src", iconURL);
 
-        })
+            //4th Day
+            var iconCode = response.list[24].weather[0].icon;
+            var iconURL = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
+            $('#tempFive').text(`Temp: ${parseInt(response.list[24].main.temp)}° F`);
+            $('#iconFive').attr('src', iconURL);
+            $('#humidFive').text(`Humidity: ${response.list[24].main.humidity}%`);
+            $('#uvIndex').html(`UV Index: ${response.value}`);
+            $('#weatherIconFive').attr("src", iconURL);
+
+            //5th Day
+            var iconCode = response.list[32].weather[0].icon;
+            var iconURL = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
+            $('#tempSix').text(`Temp: ${parseInt(response.list[32].main.temp)}° F`);
+            $('#iconSix').attr('src', iconURL);
+            $('#humidSix').text(`Humidity: ${response.list[32].main.humidity}%`);
+            $('#uvIndex').html(`UV Index: ${response.value}`);
+            $('#weatherIconSix').attr("src", iconURL);
+          })
     }
+    // geolocation
+    navigator.geolocation.getCurrentPosition(function(position) {
+      const lat = position.coords.latitude;
+      const lon = position.coords.longitude;
+      apiKey = 'f664599fcc533e3816f66a3912bb0504';
+      const queryLocationURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}%lon=${lon}&units=imperial&appid=${apiKey}`;
+      const fiveDayQueryURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}%lon=${lon}&units=imperial&appid=${apiKey}`;
+
+      //Current Weather
+      $.ajax({
+        url: queryLocationURL,
+        method: 'GET',
+      })
+      .then(function(response) {
+        //console.log(response);
+        const iconCode = response.weather[0].icon;
+        const iconURL = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
+        const city = response.name;
+        const temp = parseInt(response.main.temp);
+        const humidity = parseInt(response.main.humidity);
+        const windSpeed = parseInt(response.wind.speed);
+        const { lat } = response.coord;
+        const { lon } = response.coord;
+
+        $('#city').text(city);
+        $('#temp').text(`Temperature: ${temp}° F`);
+        $('#humidity').text(`Humidity: ${humidity} %`);
+        $('#windSpeed').text(`Wiind Speed: ${windSpeed}MPH`);
+        $('#weatherIcon').attr('src', iconURL);
+
+        $.ajax({
+          url: `https://api.openweathermap.org/data/2.5/uvi?appid=${apiKey}&lat=${lat}&lon=${lon}`,
+          method: 'GET',
+        })
+        .then(function(response) {
+          $('#uvIndex').html(`UV Index: ${response.value}`);
+        });
+
+        //5th Day
+
+
+
+
+
+
+
+
+      })
+    })
 
 
 
